b5c0e1f415333e29f8a56584efe83f74c3980b68
Zerthick <zerthick@gmail.com>
2016-11-26 15:46:23 -0500

    Removed Breed Listener

--- src/main/java/io/github/zerthick/protectionperms/events/listeners/entity/EntityListenerManager.java
+++ src/main/java/io/github/zerthick/protectionperms/events/listeners/entity/EntityListenerManager.java
@@ -38,7 +38,6 @@ public class EntityListenerManager implements ListenerManager{
         listeners.add(new DamageEntityListener());
         listeners.add(new RideEntityMountListener());
         listeners.add(new TameEntityListener());
-        listeners.add(new BreedEntityListener());
     }
 
     @Override
