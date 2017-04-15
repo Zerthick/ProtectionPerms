078c6e65c188a4cc0a4ca72d13937fa665f7f426
Zerthick <zerthick@gmail.com>
2017-04-15 15:43:00 -0400

    Bump version number.

--- src/main/java/io/github/zerthick/protectionperms/ProtectionPerms.java
+++ src/main/java/io/github/zerthick/protectionperms/ProtectionPerms.java
@@ -31,7 +31,7 @@ import org.spongepowered.api.plugin.PluginContainer;
 
 @Plugin(id = "protectionperms",
         name = "ProtectionPerms",
-        version = "0.5.1",
+        version = "0.6.0",
         description = "A simple player protection and control plugin.",
         authors = {
                 "Zerthick"
