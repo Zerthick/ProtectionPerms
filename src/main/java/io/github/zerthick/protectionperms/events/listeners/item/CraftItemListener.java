669a0c7aa7f87bc84f44109005c294476c83d196
Zerthick <zerthick@gmail.com>
2019-01-21 17:24:40 -0500

    Change item craft listener to listen to CraftItemEvent.Preview

--- src/main/java/io/github/zerthick/protectionperms/events/listeners/item/CraftItemListener.java
+++ src/main/java/io/github/zerthick/protectionperms/events/listeners/item/CraftItemListener.java
@@ -5,12 +5,11 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.Getter;
 import org.spongepowered.api.event.filter.cause.First;
-import org.spongepowered.api.event.item.inventory.ClickInventoryEvent;
+import org.spongepowered.api.event.item.inventory.CraftItemEvent;
 import org.spongepowered.api.item.ItemType;
-import org.spongepowered.api.item.inventory.Inventory;
-import org.spongepowered.api.item.inventory.InventoryArchetypes;
-import org.spongepowered.api.item.inventory.crafting.CraftingOutput;
-import org.spongepowered.api.item.inventory.query.QueryOperationTypes;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.inventory.ItemStackSnapshot;
+import org.spongepowered.api.item.inventory.transaction.SlotTransaction;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.chat.ChatTypes;
 import org.spongepowered.api.text.format.TextColors;
@@ -19,24 +18,22 @@ import org.spongepowered.api.text.format.TextColors;
 public class CraftItemListener {
 
     @Listener
-    public void onItemCraft(ClickInventoryEvent event, @First Player player, @Getter("getTargetInventory") Inventory inventory) {
+    public void onItemCraft(CraftItemEvent.Preview event, @First Player player, @Getter("getPreview") SlotTransaction preview) {
 
-        if (inventory.getArchetype() == InventoryArchetypes.PLAYER || inventory.getArchetype() == InventoryArchetypes.WORKBENCH) {
+        ItemStackSnapshot itemStack = preview.getFinal();
 
-            Inventory craftingOutputs = inventory.query(QueryOperationTypes.INVENTORY_TYPE.of(CraftingOutput.class));
+        ItemType itemType = itemStack.getType();
 
-            craftingOutputs.slots().forEach(slot -> slot.peek().ifPresent(itemStack -> {
+        if (itemType != ItemTypes.AIR) {
 
-                ItemType itemType = itemStack.getType();
-                String itemId = itemType.getId();
+            String itemId = itemType.getId();
 
-                PermHandler ph = PermHandler.getInstance();
+            PermHandler ph = PermHandler.getInstance();
 
-                if (!ph.checkPerm(player, "protectionperms.item.craft." + itemId)) {
-                    event.setCancelled(true);
-                    player.sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.RED, "You don't have permission to craft " + itemType.getName() + '!'));
-                }
-            }));
+            if (!ph.checkPerm(player, "protectionperms.item.craft." + itemId)) {
+                event.setCancelled(true);
+                player.sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.RED, "You don't have permission to craft " + itemType.getName() + '!'));
+            }
         }
     }
 }
